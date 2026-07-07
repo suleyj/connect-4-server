b3fd2ee682cd05a21406d71e376eb53fa2bc5142
suleyj <suleyj101@gmail.com>
2026-07-07 14:25:06 -0400

    feat(main): add and implement tui

--- main.go
+++ main.go
@@ -2,60 +2,141 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strings"
 
+	tea "charm.land/bubbletea/v2"
 	"github.com/suleyj/connect-4-server/board"
-	"github.com/suleyj/connect-4-server/display"
 	"github.com/suleyj/connect-4-server/game"
-	//    tea "charm.land/bubbletea/v2"
 )
 
-func main() {
+type player struct {
+	playerValue rune
+	playerName  string
+}
+
+type model struct {
+	cursor        int
+	p1            player
+	p2            player
+	gb            board.GameBoard
+	isPlayer1Turn bool
+	win           bool
+}
+
+func initialModel() model {
 	gameBoard := board.GameBoard{}
 	gameBoard.CreateBoard()
 
-	playerChoice := 0
+	return model{
+		cursor:        0,
+		p1:            player{'X', "Player 1"},
+		p2:            player{'Y', "Player 2"},
+		gb:            gameBoard,
+		isPlayer1Turn: true,
+		win:           false,
+	}
+}
 
-	playerOneTurn := true
+func (m model) Init() tea.Cmd {
+	return nil
+}
 
-	win := false
-
-	fmt.Println("Connect 4 Server")
-	fmt.Println()
-
-	for win == false {
-
-		fmt.Print("Drop a piece into a column 1 - 7: ")
-		fmt.Scan(&playerChoice)
-		fmt.Println()
-
-		playerVal := 'R'
-
-		if !playerOneTurn {
-			playerVal = 'Y'
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyPressMsg:
+		switch msg.String() {
+		case "ctrl+c", "q":
+			return m, tea.Quit
+		case "left", "h":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+		case "right", "l":
+			if m.cursor < m.gb.Columns-1 {
+				m.cursor++
+			}
+		case "enter", "space":
+			var playerVal rune
+			if m.isPlayer1Turn {
+				playerVal = m.p1.playerValue
+			} else {
+				playerVal = m.p2.playerValue
+			}
+			row, col, err := m.gb.PlaceOnBoard(m.cursor+1, playerVal)
+			if err != nil {
+				fmt.Println(err)
+			}
+			if game.IsWin(m.gb, row, col, playerVal) {
+				m.win = true
+				return m, tea.Quit
+			} else {
+				m.isPlayer1Turn = !m.isPlayer1Turn
+			}
+			m.cursor = 0
 		}
-
-		row, col, err := gameBoard.PlaceOnBoard(playerChoice, playerVal)
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
-
-		display.DisplayBoard(gameBoard.Board)
-
-		if game.IsWin(gameBoard, row, col, playerVal) {
-			win = true
-		} else {
-			playerOneTurn = !playerOneTurn
-		}
-
 	}
 
-	fmt.Println()
+	return m, nil
+}
 
-	if playerOneTurn {
-		fmt.Println("PLayer 1 is the Winner")
+func (m model) View() tea.View {
+	s := "\nConnect-4\n\n"
+
+	var currentPlayer player
+
+	if m.isPlayer1Turn {
+		currentPlayer = m.p1
 	} else {
-		fmt.Println("PLayer 2 is the Winner")
+		currentPlayer = m.p2
 	}
 
+	s += currentPlayer.playerName + "'s turn\n\n"
+
+	renderedCursor := false
+
+	// for i := m.gb.Rows - 1; i >= 0; i-- {
+	for i := range m.gb.Rows {
+		for k := range m.gb.Columns {
+			if !m.win && k == m.cursor && (i+1 > m.gb.Rows-1 || m.gb.Board[i+1][k] != ' ') && renderedCursor == false {
+				s += fmt.Sprintf("| %c ", currentPlayer.playerValue)
+				renderedCursor = true
+			} else {
+				s += fmt.Sprintf("| %c ", m.gb.Board[i][k])
+			}
+		}
+		s += fmt.Sprintln("|")
+	}
+
+	dashes := strings.Repeat("----", 7)
+	s += fmt.Sprintf("%s-\n", dashes)
+
+	s += "\nPress q to quit.\n"
+
+	v := tea.NewView(s)
+	v.WindowTitle = "Grocery List"
+
+	return v
+}
+
+func main() {
+	p := tea.NewProgram(initialModel())
+	finalModel, err := p.Run()
+	if err != nil {
+		fmt.Printf("Alas, there's been an error: %v", err)
+		os.Exit(1)
+	}
+	m := finalModel.(model)
+
+	var winner player
+
+	if m.isPlayer1Turn {
+		winner = m.p1
+	} else {
+		winner = m.p2
+	}
+
+	if m.win {
+		fmt.Println("\n" + winner.playerName + " is the winner")
+	}
 }
